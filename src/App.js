2eab2e478f22d33a3490f17c5a34bacf6674938c
Carlos Hernandez <honchkrow1995@gmail.com>
2022-08-03 16:38:45 -0600

    feat: Individual components - CardList and SearchBox

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Component } from 'react';
-import logo from './logo.svg';
+
+import CardList from './components/card-list/card-list.component';
+import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -30,21 +32,13 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input
-          className="search-box"
-          type="search"
-          placeholder="Search Monsters" 
-          onChange={ onSearchChange }/>
-
-        {
-          filteredMonsters.map( (monster) => {
-            return (
-              <div key={monster.id}>
-                <h1 >{monster.name}</h1>
-              </div>
-            )
-          })
-        }
+        
+          <SearchBox 
+            className="search-box"
+            placeholder="Search Monsters"
+            onChangeHandler={onSearchChange}
+          />
+          <CardList monsters={filteredMonsters} />
       </div>
     );
   }
